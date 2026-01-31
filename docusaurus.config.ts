0a1de499360474e9f2801acb8711bc7adc1c2a57
simminddev <simminddev@gmail.com>
2026-01-30 21:08:46 -0800

    update config file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SimMind', // Usually your GitHub org/user name.
+  organizationName: 'simminddev', // Usually your GitHub org/user name.
   projectName: 'simmind-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
